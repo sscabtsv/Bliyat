485f24d317e7d07955d5b0026524d696240f9a7c
sscabtsv <anastacioaijhenweina@gmail.com>
2026-07-21 17:46:41 +0800

    Add files via upload

---
+++ src/modules/bot/bot.cpp
@@ -0,0 +1,98 @@
+#include <algorithm>
+#include <gdr/gdr.hpp>
+#include <modules/gui/gui.hpp>
+#include <modules/hack/hack.hpp>
+
+#include "bot.hpp"
+
+using namespace geode::prelude;
+
+namespace eclipse::bot {
+
+    void Bot::setState(State state) {
+        m_state = state;
+    }
+
+    void Bot::clearInputs() {
+        m_replay.inputs.clear();
+    }
+
+    void Bot::restart() {
+        m_inputIndex = 0;
+    }
+
+    void Bot::removeInputsAfter(int frame) {
+        std::erase_if(m_replay.inputs, [&](gdr::Input<> const& input) -> bool { return input.frame > frame; });
+    }
+
+    void Bot::recordInput(int frame, PlayerButton button, bool player2, bool pressed) {
+        m_replay.inputs.emplace_back(frame, static_cast<int>(button), player2, pressed);
+    }
+
+    std::optional<gdr::Input<>> Bot::poll(int frame) {
+        if (m_inputIndex >= m_replay.inputs.size())
+            return std::nullopt;
+
+        if (m_replay.inputs[m_inputIndex].frame <= frame)
+            return m_replay.inputs[m_inputIndex++];
+
+        return std::nullopt;
+    }
+
+    std::optional<gdr::Input<>> Bot::getPrevious(bool player1) {
+        for (int i = m_inputIndex - 2; i >= 0; i--) {
+            if (m_replay.inputs[i].player2 == !player1)
+                return m_replay.inputs[i];
+        }
+
+        return std::nullopt;
+    }
+
+    void Bot::setLevelInfo(gdr::Level const& levelInfo) {
+        m_replay.levelInfo = levelInfo;
+    }
+
+    void Bot::setPlatformer(bool platformer) {
+        m_replay.platformer = platformer;
+    }
+
+    Result<> Bot::save(std::filesystem::path const& path) {
+        m_replay.author = utils::get<GJAccountManager>()->m_username;
+        m_replay.duration = !m_replay.inputs.empty() ? m_replay.inputs[m_replay.inputs.size() - 1].frame / m_replay.framerate : 0;
+
+        auto res = m_replay.exportData();
+
+        if(res.isErr())
+            return Err(std::move(res).unwrapErr());
+
+        auto data = std::move(res).unwrap();
+        return file::writeBinarySafe(path, data);
+    }
+
+    Result<> Bot::load(std::filesystem::path const& path) {
+        auto res = file::readBinary(path);
+        if (res.isErr())
+            return Err(std::move(res).unwrapErr());
+
+        auto fileData = std::move(res).unwrap();
+        return load(fileData);
+    }
+
+    Result<> Bot::load(std::span<uint8_t> data) {
+        if (data.size() < 3)
+            return Err("Data is too small to be a valid replay");
+
+        gdr::Result<BotReplay> res = gdr::Err<BotReplay>("");
+        if(std::equal(data.begin(), data.begin() + 3, "GDR"))
+            res = BotReplay::importData(data);
+        else
+            res = gdr::convert<BotReplay, gdr::Input<>>(data);
+
+        if (res.isErr())
+            return Err(std::move(res).unwrapErr());
+
+        m_replay = std::move(res).unwrap();
+
+        return Ok();
+    }
+}
---
+++ src/modules/bot/bot.hpp
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <gdr_convert.hpp>
+
+namespace eclipse::bot {
+
+    enum class State {
+        DISABLED,
+        RECORD,
+        PLAYBACK
+    };
+
+    struct BotReplay : gdr::Replay<BotReplay, gdr::Input<>> {
+        BotReplay() : Replay("EclipseBot", 1) {}
+    };
+
+    class Bot {
+    public:
+
+        /// @brief Set the current state of the bot.
+        /// @param state State of the bot.
+        void setState(State state);
+
+        /// @brief Get the current state of the bot.
+        [[nodiscard]] State getState() const { return m_state; }
+
+        [[nodiscard]] size_t getInputCount() const { return m_replay.inputs.size(); }
+
+        [[nodiscard]] float getFramerate() const { return m_replay.framerate; }
+
+        void setFramerate(float framerate) { m_replay.framerate = framerate; }
+
+        /// @brief Removes all inputs from the currently loaded replay.
+        void clearInputs();
+
+        /// @brief Restarts the replay from the beginning.
+        void restart();
+
+        /// @brief Removes all inputs from the currently loaded replay, up to the given frame.
+        /// @param frame Frame to stop at.
+        void removeInputsAfter(int frame);
+
+        /// @brief Add an input to the currently loaded replay.
+        void recordInput(int frame, PlayerButton button, bool player2, bool pressed);
+
+        /// @brief Get the next available input from the currently loaded replay.
+        /// @param frame The current frame of the playback.
+        /// @return The next input from the currently loaded replay, if available.
+        [[nodiscard]] std::optional<gdr::Input<>> poll(int frame);
+
+        /// @brief Saves the current replay.
+        /// @param path Path to save the replay to.
+        geode::Result<> save(std::filesystem::path const& path);
+
+        /// @brief Loads a replay from the given path.
+        /// @param path Path to load the replay from.
+        geode::Result<> load(std::filesystem::path const& path);
+
+        /// @brief Loads a replay from a data buffer.
+        /// @param data Data to load the replay from.
+        geode::Result<> load(std::span<uint8_t> data);
+
+        /// @brief Sets the level info.
+        /// @param levelInfo The level info.
+        void setLevelInfo(gdr::Level const& levelInfo);
+
+        /// @brief Sets platformer.
+        /// @param platformer Platformer.
+        void setPlatformer(bool platformer);
+
+        [[nodiscard]] std::optional<gdr::Input<>> getPrevious(bool player1);
+
+    protected:
+        State m_state = State::DISABLED;
+        BotReplay m_replay;
+        uint32_t m_inputIndex = 0;
+    };
+
+}
